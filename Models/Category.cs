8a131b8fa869d43dd97861eb1b9b67c1369fb7a2
Sean Malan <sean.malan@developersinstitute.co.nz>
2023-04-27 21:16:01 +1200

    Adds form validation

--- Models/Category.cs
+++ Models/Category.cs
@@ -9,12 +9,14 @@ namespace Expense_Tracker.Models
         public int CategoryId { get; set; }
 
         [Column(TypeName = "nvarchar(50)")]
+        [Required(ErrorMessage = "This field is required.")]
         public string Title { get; set; }
 
         [Column(TypeName = "nvarchar(5)")]
         public string Icon { get; set; } = "";
 
         [Column(TypeName = "nvarchar(10)")]
+        [Required(ErrorMessage = "This field is required.")]
         public string Type { get; set; }  = "Expense";
 
         [NotMapped]
--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -8,9 +8,11 @@ namespace Expense_Tracker.Models
         [Key]
         public int TransactionId { get; set; }
 
+        [Range(0, int.MaxValue, ErrorMessage ="Please select a category")]
         public int CategoryId { get; set; }
         public Category? Category { get; set; }
 
+        [Range(0, int.MaxValue, ErrorMessage = "Amount must be greater than $0")]
         public int Amount { get; set; }
 
         [Column(TypeName = "nvarchar(75)")]
